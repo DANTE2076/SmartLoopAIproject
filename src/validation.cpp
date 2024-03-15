e44d2eab900a2d319f8f2400bc11cec2d9c32c78
DANTE2076 <161814144+DANTE2076@users.noreply.github.com>
2024-03-15 20:42:58 +0100

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1121,45 +1121,46 @@ NOTE:   unlike bitcoin we are using PREVIOUS block height here,
 */
 static std::pair<CAmount, CAmount> GetBlockSubsidyHelper(int nPrevBits, int nPrevHeight, const Consensus::Params& consensusParams, bool fV20Active)
 {
-    double dDiff;
-    CAmount nSubsidyBase;
+    // double dDiff;
+    CAmount nSubsidyBase = 500;
 
-    if (nPrevHeight <= 4500 && Params().NetworkIDString() == CBaseChainParams::MAIN) {
-        /* a bug which caused diff to not be correctly calculated */
-        dDiff = (double)0x0000ffff / (double)(nPrevBits & 0x00ffffff);
-    } else {
-        dDiff = ConvertBitsToDouble(nPrevBits);
-    }
+    // if (nPrevHeight <= 4500 && Params().NetworkIDString() == CBaseChainParams::MAIN) {
+    //     /* a bug which caused diff to not be correctly calculated */
+    //     dDiff = (double)0x0000ffff / (double)(nPrevBits & 0x00ffffff);
+    // } else {
+    //     dDiff = ConvertBitsToDouble(nPrevBits);
+    // }
 
-    const bool isDevnet = Params().NetworkIDString() == CBaseChainParams::DEVNET;
-    const bool force_fixed_base_subsidy = fV20Active || (isDevnet && nPrevHeight >= consensusParams.nHighSubsidyBlocks);
-    if (force_fixed_base_subsidy) {
+    // const bool isDevnet = Params().NetworkIDString() == CBaseChainParams::DEVNET;
+    // const bool force_fixed_base_subsidy = fV20Active || (isDevnet && nPrevHeight >= consensusParams.nHighSubsidyBlocks);
+    // if (force_fixed_base_subsidy) {
         // Originally, nSubsidyBase calculations relied on difficulty. Once Platform is live,
         // it must be able to calculate platformReward. However, we don't want it to constantly
         // get blocks difficulty from the payment chain, so we set the nSubsidyBase to a fixed
         // value starting from V20 activation. Note, that it doesn't affect mainnet really
         // because blocks difficulty there is very high already.
         // Devnets get fixed nSubsidyBase starting from nHighSubsidyBlocks to better mimic mainnet.
-        nSubsidyBase = 5;
-    } else if (nPrevHeight < 5465) {
-        // Early ages...
-        // 1111/((x+1)^2)
-        nSubsidyBase = (1111.0 / (pow((dDiff+1.0),2.0)));
-        if(nSubsidyBase > 500) nSubsidyBase = 500;
-        else if(nSubsidyBase < 1) nSubsidyBase = 1;
-    } else if (nPrevHeight < 17000 || (dDiff <= 75 && nPrevHeight < 24000)) {
-        // CPU mining era
-        // 11111/(((x+51)/6)^2)
-        nSubsidyBase = (11111.0 / (pow((dDiff+51.0)/6.0,2.0)));
-        if(nSubsidyBase > 500) nSubsidyBase = 500;
-        else if(nSubsidyBase < 25) nSubsidyBase = 25;
-    } else {
-        // GPU/ASIC mining era
-        // 2222222/(((x+2600)/9)^2)
-        nSubsidyBase = (2222222.0 / (pow((dDiff+2600.0)/9.0,2.0)));
-        if(nSubsidyBase > 25) nSubsidyBase = 25;
-        else if(nSubsidyBase < 5) nSubsidyBase = 5;
-    }
+        // nSubsidyBase = 5;
+    // }
+    // } else if (nPrevHeight < 5465) {
+    //     // Early ages...
+    //     // 1111/((x+1)^2)
+    //     nSubsidyBase = (1111.0 / (pow((dDiff+1.0),2.0)));
+    //     if(nSubsidyBase > 500) nSubsidyBase = 500;
+    //     else if(nSubsidyBase < 1) nSubsidyBase = 1;
+    // } else if (nPrevHeight < 17000 || (dDiff <= 75 && nPrevHeight < 24000)) {
+    //     // CPU mining era
+    //     // 11111/(((x+51)/6)^2)
+    //     nSubsidyBase = (11111.0 / (pow((dDiff+51.0)/6.0,2.0)));
+    //     if(nSubsidyBase > 500) nSubsidyBase = 500;
+    //     else if(nSubsidyBase < 25) nSubsidyBase = 25;
+    // } else {
+    //     // GPU/ASIC mining era
+    //     // 2222222/(((x+2600)/9)^2)
+    //     nSubsidyBase = (2222222.0 / (pow((dDiff+2600.0)/9.0,2.0)));
+    //     if(nSubsidyBase > 25) nSubsidyBase = 25;
+    //     else if(nSubsidyBase < 5) nSubsidyBase = 5;
+    // }
 
     CAmount nSubsidy = nSubsidyBase * COIN;
 
@@ -1168,10 +1169,10 @@ static std::pair<CAmount, CAmount> GetBlockSubsidyHelper(int nPrevBits, int nPre
         nSubsidy -= nSubsidy/14;
     }
 
-    if (nPrevHeight < consensusParams.nHighSubsidyBlocks) {
-        assert(isDevnet);
-        nSubsidy *= consensusParams.nHighSubsidyFactor;
-    }
+    // if (nPrevHeight < consensusParams.nHighSubsidyBlocks) {
+    //     assert(isDevnet);
+    //     nSubsidy *= consensusParams.nHighSubsidyFactor;
+    // }
 
     CAmount nSuperblockPart{};
     // Hard fork to reduce the block reward by 10 extra percent (allowing budget/superblocks)
@@ -1179,7 +1180,12 @@ static std::pair<CAmount, CAmount> GetBlockSubsidyHelper(int nPrevBits, int nPre
         // Once v20 is active, the treasury is 20% instead of 10%
         nSuperblockPart = nSubsidy / (fV20Active ? 5 : 10);
     }
-    return {nSubsidy - nSuperblockPart, nSuperblockPart};
+
+    if (nPrevHeight < 5400) {
+          return {nSubsidy - nSuperblockPart, nSuperblockPart};
+        } else {
+          return {nSubsidy, 0};
+        }
 }
 
 CAmount GetSuperblockSubsidyInner(int nPrevBits, int nPrevHeight, const Consensus::Params& consensusParams, bool fV20Active)
@@ -1204,6 +1210,13 @@ CAmount GetBlockSubsidy(const CBlockIndex* const pindex, const Consensus::Params
 CAmount GetMasternodePayment(int nHeight, CAmount blockValue, bool fV20Active)
 {
     CAmount ret = blockValue * 0.5;
+
+    if (nHeight < 5400) {
+      return static_cast<CAmount>(blockValue * 1);
+    } else {
+      return ret;
+    }
+
     const int nReallocActivationHeight = Params().GetConsensus().BRRHeight;
 
 
