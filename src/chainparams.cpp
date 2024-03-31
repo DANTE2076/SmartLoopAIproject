55647e447925833c9593165ed8e164d6cf240b9a
DANTE2076 <161814144+DANTE2076@users.noreply.github.com>
2024-03-31 14:19:34 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -277,10 +277,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("node3.walletbuilders.com",false);
-        vSeeds.emplace_back("seed1.smartloopai.net",false);
-        vSeeds.emplace_back("seed3.smartloopai.net",false);
-        vSeeds.emplace_back("seed2.smartloopai.net",false);
+        vSeeds.emplace_back("185.203.116.146",false);
+//        vSeeds.emplace_back("seed3.smartloopai.net",false);
+//        vSeeds.emplace_back("seed2.smartloopai.net",false);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,64);
